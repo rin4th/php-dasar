5a836ed86c385d734fdac7e1247242f2293c86ab
rin4th <rizalnur.art@gmail.com>
2022-05-03 12:48:22 +0700

    add pert12

---
+++ pert12/functions.php
@@ -0,0 +1,72 @@
+<?php
+$db = mysqli_connect("localhost", "rin", "zal0192++!!", "phpdasar");
+
+// connect mysqli
+function query($query){
+  global $db;
+  $result = mysqli_query($db, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function tambah($data){
+  global $db;
+  
+// cek apakah tombol submit sudah ditekan atau tidak
+  $nim     = htmlspecialchars($data["nim"]);
+  $nama    = htmlspecialchars($data["nama"]);
+  $jurusan = htmlspecialchars($data["jurusan"]);
+  $gambar  = htmlspecialchars($data["gambar"]);
+
+  // query untuk menambahkan data
+  $insert = "INSERT INTO mahasiswa
+    (nama, nim, jurusan, gambar)
+  VALUES(
+  '$nama', '$nim', '$jurusan', '$gambar');";
+
+  mysqli_query($db, $insert);
+
+  return mysqli_affected_rows($db);
+}
+
+function hapus($id){
+  global $db;
+
+  return mysqli_query($db, "DELETE FROM mahasiswa WHERE id = $id");
+  }
+
+function update($data){
+  global $db;
+  
+// cek apakah tombol submit sudah ditekan atau tidak    
+  $id       = $data["id"];
+  $nim     = htmlspecialchars($data["nim"]);
+  $nama    = htmlspecialchars($data["nama"]);
+  $jurusan = htmlspecialchars($data["jurusan"]);
+  $gambar  = htmlspecialchars($data["gambar"]);
+
+  // query untuk update data
+  $insert = "UPDATE mahasiswa SET
+                nim = '$nim',
+                nama = '$nama',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+            WHERE id = $id";
+
+  mysqli_query($db, $insert);
+
+  return mysqli_affected_rows($db);
+}
+
+function search($keyword){
+    $query = "SELECT * FROM mahasiswa WHERE
+                nama LIKE '%$keyword%' or
+                nim LIKE '%$keyword%' or
+                jurusan LIKE '%$keyword%'";
+    
+    return query($query);
+}
+?>
